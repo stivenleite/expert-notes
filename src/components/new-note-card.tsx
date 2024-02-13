05120060183d15b901911aa894e0ee18e23b06ca
Stiven Leite <stivenleite@gmail.com>
2024-02-13 12:34:29 -0400

    project finished

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -7,8 +7,11 @@ interface NewNoteCardProps {
   onNoteCreated : (content: string) => void
 }
 
+let speechRecognition: SpeechRecognition | null = null
+
 export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
     const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
+    const [isRecording, setIsRecording] = useState(false)
     const [content, setContent] = useState('')
 
     function handleTextEditor() {
@@ -26,12 +29,63 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
     function handleSaveNote(event: FormEvent) {
       event.preventDefault()
 
+      if(content === '') {return}
+
       onNoteCreated(content)
 
       setContent('')
       setShouldShowOnboarding(true)
 
       toast.success('Note created successfully!')
+    }
+
+    function handleStartRecording() {
+      const isSpeechRecognitionAPIAvailable = 'SpeechRecognition' in window || 'webkitSpeechRecognition' in window
+      
+      if(!isSpeechRecognitionAPIAvailable) {
+        alert("Unfortunately, this browser do not support recording API!")
+        return
+      }
+      
+      setIsRecording(true)
+      setShouldShowOnboarding(false)
+      
+      const SpeechRecognitionAPI = window.SpeechRecognition || window.webkitSpeechRecognition
+
+      speechRecognition = new SpeechRecognitionAPI()
+
+      speechRecognition.lang = 'en-US'
+      speechRecognition.continuous = true
+      speechRecognition.maxAlternatives = 1
+      speechRecognition.interimResults = true
+
+      speechRecognition.onresult = (event) => {
+        const transcription = Array.from(event.results).reduce((text, result) => {
+          return text.concat(result[0].transcript)
+        }, '')
+
+        setContent(transcription)
+      }
+
+      speechRecognition.onerror = (event) => {
+        console.log(event)
+      }
+
+      speechRecognition.start()
+    }
+
+    function handleStopRecording() {
+      setIsRecording(false)
+
+      if(speechRecognition != null) {
+        speechRecognition.stop()
+      }
+    }
+
+    function handleCloseButton() {
+      handleStopRecording()
+
+      setShouldShowOnboarding(true)
     }
 
     return (
@@ -41,18 +95,18 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
               Add note
             </span>
             <p className="text-sm leading-6 text-slate-400">
-              Record an audio note to be automatically converted into text.
+              Record an audio note to be automatically converted into text or just type it.
             </p>
           </Dialog.Trigger>
 
           <Dialog.Portal>
             <Dialog.Overlay className="inset-0 fixed bg-black/50"/>
-            <Dialog.DialogContent className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none overflow-hidden">
+            <Dialog.DialogContent className="fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none overflow-hidden">
               <Dialog.Close className="absolute right-0 top-0 p-1.5 bg-slate-800 text-slate-400 hover:text-slate-100">
-                <X className="size-5" />
+                <X className="size-5" onClick={handleCloseButton} />
               </Dialog.Close>
 
-              <form onSubmit={handleSaveNote} className="flex flex-col flex-1">
+              <form className="flex flex-col flex-1">
                 <div className="flex flex-1 flex-col gap-3 p-5  ">
                   <span className="text-sm font-medium text-slate-300">
                     Add note
@@ -60,7 +114,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
                   {
                     shouldShowOnboarding ? (
                       <p className="text-sm leading-6 text-slate-400">
-                        <button className="font-medium text-lime-400 hover:underline">Record</button> and audio or <button onClick={handleTextEditor} className="font-medium text-lime-400 hover:underline">type</button> a text.
+                        <button type='button' onClick={handleStartRecording} className="font-medium text-lime-400 hover:underline">Record</button> an audio or <button type='button' onClick={handleTextEditor} className="font-medium text-lime-400 hover:underline">type</button> a text.
                       </p>
 
                     ) : ( 
@@ -74,12 +128,27 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
                   }
                 </div>
 
-                <button 
-                  type='submit'
-                  className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 font-medium outline-none hover:bg-lime-500  "
-                >
-                  Salvar nota
-                </button>
+                {
+                  isRecording ? (
+                    <button 
+                      type='button'
+                      onClick={handleStopRecording}
+                      className="w-full flex items-center justify-center gap-2 bg-slate-900 py-4 text-center text-sm text-slate-300 font-medium outline-none hover:text-slate-100  "
+                    >
+                      <div className="size-3 rounded-full bg-red-500 animate-pulse" />
+                      Recording (Click to stop)
+                    </button>
+                  ) : (
+                    <button 
+                      type='button'
+                      onClick={handleSaveNote}
+                      className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 font-medium outline-none hover:bg-lime-500  "
+                    >
+                      Save note
+                    </button>
+                  )
+                }
+
               </form>
             </Dialog.DialogContent>
           </Dialog.Portal>
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -4,12 +4,14 @@ import { X } from 'lucide-react'
 
 interface NoteCardProps {
   note: {
+    id: string
     date: Date
     content: string
   }
+  onNoteDeleted: (id: string) => void
 }
 
-export function NoteCard({ note }: NoteCardProps) {
+export function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
     return(
       <Dialog.Root>
         <Dialog.Trigger className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
@@ -25,7 +27,7 @@ export function NoteCard({ note }: NoteCardProps) {
 
         <Dialog.Portal>
           <Dialog.Overlay className="inset-0 fixed bg-black/50"/>
-          <Dialog.DialogContent className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none overflow-hidden">
+          <Dialog.DialogContent className="fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none overflow-hidden">
             <Dialog.Close className="absolute right-0 top-0 p-1.5 bg-slate-800 text-slate-400 hover:text-slate-100">
                <X className="size-5" />
             </Dialog.Close>
@@ -41,6 +43,7 @@ export function NoteCard({ note }: NoteCardProps) {
 
             <button 
               type='button'
+              onClick={() => onNoteDeleted(note.id)}
               className="w-full bg-slate-800 py-4 text-center text-sm text-red-400 font-medium outline-none hover:underline "
             >
               Delete
